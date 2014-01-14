93c19d97b2350cbd6a4e7487c730e0c295508d94
Zheng Shao <axot@msn.com>
2014-01-14 23:31:47 +0900

    Initial Commit

---
+++ imem/AXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AXAppDelegate.h
+//  imem
+//
+//  Created by Zheng Shao on 1/14/14.
+//  Copyright (c) 2014 axot.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ imemTests/imemTests.m
@@ -0,0 +1,34 @@
+//
+//  imemTests.m
+//  imemTests
+//
+//  Created by Zheng Shao on 1/14/14.
+//  Copyright (c) 2014 axot.org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface imemTests : XCTestCase
+
+@end
+
+@implementation imemTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
